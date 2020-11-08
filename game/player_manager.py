1d9da7ca545d33238406c674ded5577929f640ce
Tamás Bunth <btomi96@gmail.com>
2020-11-08 13:32:34 +0100

    Clean player manager

--- game/player_manager.py
+++ game/player_manager.py
@@ -8,33 +8,30 @@ from game.utils.move_verdict import MoveVerdict
 from game.utils.position import Position
 
 _dir_func_map = {
-    Direction.DOWN : lambda pos: pos + Position(0, 1),
+    Direction.DOWN: lambda pos: pos + Position(0, 1),
     Direction.UP: lambda pos: pos + Position(0, -1),
     Direction.RIGHT: lambda pos: pos + Position(1, 0),
     Direction.LEFT: lambda pos: pos + Position(-1, 0),
 }
 
 
-
 class PlayerManager:
     def __init__(self, map: GameMap):
         self._map = map
 
-    def move_all_players(self, dir: Direction):
+    def move_all_players(self, direction: Direction):
         player_info = [p for p in self._map.getPlayers() if p[0].is_alive()]
-        logging.debug("Player info: {}".format(player_info))
         for player, pos in player_info:
-            self.step_with(player, pos, dir)
+            self.step_with(player, pos, direction)
 
-    def step_with(self, p: Player, current_position: Position, dir: Direction):
+    def step_with(self, p: Player, current_position: Position, direction: Direction):
         verdict = MoveVerdict.MOVE
         prev_pos = current_position
-        move_info = MoveInfo(direction=dir, momentum=0)
+        move_info = MoveInfo(direction=direction, momentum=0)
         while verdict == MoveVerdict.MOVE:
-            new_pos = _dir_func_map.get(dir)(prev_pos)
+            new_pos = _dir_func_map.get(direction)(prev_pos)
             cell_to_interact = self._map.block(new_pos)
             verdict = cell_to_interact.before_step(p, move_info)
-            logging.debug(f"Can step verdict: {verdict} from pos: {new_pos}")
             if verdict == MoveVerdict.MOVE:
                 logging.info(f"Player steps from: {prev_pos} to {new_pos}")
                 self._map.move(prev_pos, new_pos, p)
