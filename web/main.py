7b8c41c08f257ec25e62ce35c3b088554c7f75f3
Tamás Bunth <btomi96@gmail.com>
2021-04-17 13:29:47 +0200

    Add web client/server

---
+++ web/main.py
@@ -0,0 +1,50 @@
+import asyncio
+import json
+import logging
+import os
+import pathlib
+
+import aiofiles as aiofiles
+import websockets
+
+from game.gamemap import GameMap
+from game.json_import.map_schema import MapSchema
+
+FORMAT = "[%(levelname)5s - %(filename)s:%(lineno)s::%(funcName)s] %(message)s"
+logging.basicConfig(format=FORMAT, level=logging.DEBUG)
+logging.getLogger("PIL").setLevel(logging.WARNING)
+
+async def open_map(file_name: str) -> GameMap:
+    # Load a test data for debugging
+    async with aiofiles.open(
+            os.path.join(pathlib.Path(__file__).parent.parent, "example_maps",
+                         file_name)) as fp:
+        data = await fp.read()
+        print(type(data))
+        json_data = json.loads(data)
+
+    schema = MapSchema()
+    map_model = schema.load(json_data)
+
+    return map_model
+
+
+async def hello(websocket, path: str):
+    logging.info("Received connection")
+
+    # send map
+    map_model = await open_map("boulder.json")
+    schema = MapSchema()
+    back_to_json = schema.dumps(map_model)
+    await websocket.send(back_to_json)
+
+def main():
+    HOST, PORT = "localhost", 8765
+    start_server = websockets.serve(hello, HOST, PORT)
+    logging.info(f"Serving at {HOST}:{PORT}")
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
+
+
+if __name__ == "__main__":
+    main()
